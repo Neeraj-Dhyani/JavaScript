73046f0a33f26bd2cf07029067537884402940f6
Neeraj-Dhyani <neerajdhyani47@gmail.com>
2024-08-27 12:08:15 +0530

    javascript functions

---
+++ function/function.js
@@ -0,0 +1,35 @@
+// function calculator(num1, ope, num2){
+//     if(ope=="+"){
+//         console.log(num1+num2);
+//     }
+//     else if(ope=="-"){
+//         console.log(num1-num2);
+//         retur0;
+//     }
+
+// }
+
+// calculator(8, "+", 7);
+
+// function pyramid(n){
+//     for(let i=1; i<=n; i++){
+//         for(let j=1; j<=i; j++){
+//             process.stdout.write("*");
+//         }
+//         console.log();
+
+//     }
+
+// }
+
+// pyramid(4);
+for(let i=1; i<=5; i++){
+    for(let j=1; j<=5; j++){
+        if(i==1||j==5){
+            process.stdout.write("*"+"\t");
+       
+    
+    }
+    console.log();
+    
+}
